2cd176435795bbb47a0f994cde13f1a6e30e843a
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-09-09 17:06:40 -0500

    corregido el import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,9 +1,15 @@
-
+INSERT INTO regiones(id, nombre) VALUES (1, 'Sudamerica');
+INSERT INTO regiones(id, nombre) VALUES (2, 'Centro America');
+INSERT INTO regiones(id, nombre) VALUES (3, 'Norteamerica');
+INSERT INTO regiones(id, nombre) VALUES (4, 'Europa');
+INSERT INTO regiones(id, nombre) VALUES (5, 'Asia');
+INSERT INTO regiones(id, nombre) VALUES (6, 'Africa');
+INSERT INTO regiones(id, nombre) VALUES (7, 'Oceania');
+INSERT INTO regiones(id, nombre) VALUES (8, 'Antartida');
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) values ('miguel','$2a$10$2ZbESYHsnMm61kNys0nFqeyVvphRdgJQWHF18F6PUpFSweKfLNHU2',1,'Miguel Angel','Chinchay','miguel@gmail.com');
 INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) values ('admin','$2a$10$dtB6css6tfT4BlDK52YTA.htfqdL68kDBO2rl6Y5akxJp28krm6r.',1,'Administrador', 'ApeAdmin','admin@gmail.com');
 INSERT INTO roles (nombre) VALUES ('ROLE_USER'), ('ROLE_ADMIN');
 INSERT INTO usuarios_roles (usuario_id, roles_id) VALUES (1,1), (2,2), (2,1);
-INSERT INTO db_proy_senior.regiones(nombre) values ('Sudamérica'),('Centroamérica'),('Norteamérica'),('Europa'),('Asia'),('Africa'),('Oceanía'),('Antártida');
 
 INSERT INTO clientes (region_id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzmán', 'profesor@bolsadeideas.com', NOW());
 
